046347c52eb610fcab465151ea1791753f8a6e13
mattiadebonis <mattia.debonis@gmail.com>
2021-04-27 16:17:17 +0200

    Html, variabili e commenti js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// chiedere i chilometri da percorrere e l'età all'utente
+
+//dati dell'utente
+var etaUtente = prompt("Quanti anni hai?");
+var chilometriUtente = prompt("Quanti chilometri vuoi percorrere?");
+var maggioreEta = 18;
+
+//parametri per il calcolo dei prodotti
+var prezzoChilometro = 0.21;
+var scontoMinorenni = 40;
+var scontoAnziani = 65;
+
+//calcolare il prezzo del biglietto (0,21 al chilometro)
+
+//se minorenni applicare sconto del 20%
+
+//se over 65 applicare sconto del 40%
+
+//prezzo con massimo due decimali 
